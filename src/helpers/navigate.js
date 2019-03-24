07e6bd70c659ccd4520de5cb3ad848a65891ae07
Andrew Davies <globalroo@hotmail.com>
2019-03-24 23:21:27 +0000

    Remove / from routes

--- src/helpers/navigate.js
+++ src/helpers/navigate.js
@@ -4,13 +4,13 @@ import { navigate } from "@reach/router";
 export const navigateTo = ({ media_type, id }) => {
 	switch (media_type) {
 		case MEDIA_TYPE.TV:
-			navigate(`/tv/${id}`);
+			navigate(`tv/${id}`);
 			break;
 		case MEDIA_TYPE.PERSON:
-			navigate(`/person/${id}`);
+			navigate(`person/${id}`);
 			break;
 		case MEDIA_TYPE.MOVIE:
-			navigate(`/movie/${id}`);
+			navigate(`movie/${id}`);
 			break;
 		default:
 			console.error("Can't handle this type of media!");
